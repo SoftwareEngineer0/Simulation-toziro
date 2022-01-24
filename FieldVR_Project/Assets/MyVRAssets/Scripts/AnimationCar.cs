f77cbfd98726042b4dbfa52c175cdc6d673318b5
Noot-dev <developerteam888@outlook.com>
2022-01-23 23:24:38 -0800

    update car model

--- FieldVR_Project/Assets/MyVRAssets/Scripts/AnimationCar.cs
+++ FieldVR_Project/Assets/MyVRAssets/Scripts/AnimationCar.cs
@@ -10,23 +10,23 @@ namespace MyVR_Assets
         public int GenerateNum;
         public float Speed_km;
 
-        public AnimationClip Genkyo01;
-        public AnimationClip Genkyo02;
+        // public AnimationClip Genkyo01;
+        // public AnimationClip Genkyo02;
 
-        public AnimationClip Keiaku01;
-        public AnimationClip Keiaku02;
+        // public AnimationClip Keiaku01;
+        // public AnimationClip Keiaku02;
 
         public AnimationClip[] CarAnimClip;
         public int[] AnimationClipWeight;
         public GameObject[] CarModel;
 
-        public Text ObjectSwitchButtonText;
+        // public Text ObjectSwitchButtonText;
 
-        public Toggle CarToggle;
+        // public Toggle CarToggle;
 
         void Start()
         {
-          //CarGenerate_A();
+          CarGenerate_A();
         }
 
         public void CarGenerate_A()
@@ -37,8 +37,11 @@ namespace MyVR_Assets
                 GenerateClone.transform.localScale = new Vector3(1.1f,1.0f,1.0f);
                 GenerateClone.tag = "AnimationCar";
                 GenerateClone.transform.parent = this.transform;
+                GenerateClone.transform.parent.position = new Vector3(this.transform.position.x, 0.81f, this.transform.position.z);
 
                 Animation CarAnim_A = GenerateClone.AddComponent<Animation>();
+                Debug.Log("CarGererate_A:" + ClipWeight.GetRandomIndex(AnimationClipWeight) + ":" + CarAnimClip[0]);
+
                 CarAnim_A.AddClip(CarAnimClip[ClipWeight.GetRandomIndex(AnimationClipWeight)], "CarAnim_A");
                 CarAnim_A["CarAnim_A"].speed = Speed_km * 0.01f;
                 CarAnim_A["CarAnim_A"].normalizedTime = Random.Range(0, 1f);
@@ -67,43 +70,44 @@ namespace MyVR_Assets
 
         public void AnimationChange()
         {
-            if (ObjectSwitchButtonText.text == "現況")
-            {
-                CarAnimClip[0] = Genkyo01;
-                CarAnimClip[1] = Genkyo02;
+            // if (ObjectSwitchButtonText.text == "現況")
+            // {
+            //     CarAnimClip[0] = Genkyo01;
+            //     CarAnimClip[1] = Genkyo02;
 
-                var DestroyCar = GameObject.FindGameObjectsWithTag("AnimationCar");
+            //     var DestroyCar = GameObject.FindGameObjectsWithTag("AnimationCar");
 
-                foreach (var Obj in DestroyCar)
-                {
-                    Destroy(Obj);
-                }
+            //     foreach (var Obj in DestroyCar)
+            //     {
+            //         Destroy(Obj);
+            //     }
 
-                CarGenerate_A();
-            }
-            else if(ObjectSwitchButtonText.text == "仮人道橋設置")
-            {
-                var DestroyCar = GameObject.FindGameObjectsWithTag("AnimationCar");
+            //     CarGenerate_A();
+            // }
+            // else if(ObjectSwitchButtonText.text == "仮人道橋設置")
+            // {
+            //     var DestroyCar = GameObject.FindGameObjectsWithTag("AnimationCar");
 
-                foreach (var Obj in DestroyCar)
-                {
-                    Destroy(Obj);
-                }
-            }
-            else if(ObjectSwitchButtonText.text == "工事完了後")
-            {
-                CarAnimClip[0] = Keiaku01;
-                CarAnimClip[1] = Keiaku02;
+            //     foreach (var Obj in DestroyCar)
+            //     {
+            //         Destroy(Obj);
+            //     }
+            // }
+            // else if(ObjectSwitchButtonText.text == "工事完了後")
+            // {
+            //     CarAnimClip[0] = Keiaku01;
+            //     CarAnimClip[1] = Keiaku02;
 
-                var DestroyCar = GameObject.FindGameObjectsWithTag("AnimationCar");
+            //     var DestroyCar = GameObject.FindGameObjectsWithTag("AnimationCar");
 
-                foreach (var Obj in DestroyCar)
-                {
-                    Destroy(Obj);
-                }
+            //     foreach (var Obj in DestroyCar)
+            //     {
+            //         Destroy(Obj);
+            //     }
 
-                CarGenerate_A();
-            }
+            //     CarGenerate_A();
+            // }
+            CarGenerate_A();
         }
 
 
@@ -117,27 +121,27 @@ namespace MyVR_Assets
                 Destroy(Obj);
             }
 
-            if (CarToggle.isOn)
-            {
-                StartCoroutine("GenerateAgain");
-            }
+            // if (CarToggle.isOn)
+            // {
+            //     StartCoroutine("GenerateAgain");
+            // }
         }
 
         IEnumerator GenerateAgain()
         {
             yield return null;
-
-            if (ObjectSwitchButtonText.text == "完成")//ObjectSwitchButton.GetComponent<ObjectSwitch>().Count == 1)
-            {
-                CarGenerate_A();
-                //CarKyoutsuGenerate();
-                //CarGenkyoGenerate();
-            }
-            //else if (ObjectSwitchButton.GetComponent<ObjectSwitch>().Count == 2)
-            //{
-            //CarKyoutsuGenerate();
-            //CarKanseiGenerate();
-            //}
+            CarGenerate_A();
+            // if (ObjectSwitchButtonText.text == "完成")//ObjectSwitchButton.GetComponent<ObjectSwitch>().Count == 1)
+            // {
+            //     CarGenerate_A();
+            //     //CarKyoutsuGenerate();
+            //     //CarGenkyoGenerate();
+            // }
+            // //else if (ObjectSwitchButton.GetComponent<ObjectSwitch>().Count == 2)
+            // //{
+            // //CarKyoutsuGenerate();
+            // //CarKanseiGenerate();
+            // //}
         }
 
         //アニメーションのルートをウエイトを付けて返す
